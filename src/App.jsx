513bc384fd3e4c4da54c714cf13bcbfb050b4c5f
Foshati <amirrezafoshati@gmail.com>
2024-06-25 23:55:06 +0330

    fix counter with zustand 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import useCounterStore from "./store/useCounterStore";
+import Counter from "./components/Counter";
 
 function App() {
-  const { count, increment, decrement, reset } = useCounterStore();
   return (
     <>
-      <div className="flex flex-col w-3/12 gap-6 p-4 m-4">
-        <h1 className="p-4 rounded-lg bg-slate-500">Count: {count}</h1>
-        <button className="btn" onClick={increment}>
-          +Increment
-        </button>
-        <button className="btn" onClick={decrement}>
-          -Decrement
-        </button>
-        <button className="btn" onClick={reset}>
-          Reset
-        </button>
-      </div>
+      <Counter />
     </>
   );
 }
---
+++ src/components/Counter.jsx
@@ -0,0 +1,24 @@
+import useCounterStore from "../store/useCounterStore";
+
+export default function Counter() {
+  const { count, increment, decrement, reset } = useCounterStore();
+
+  return (
+    <div>
+      <div className="flex flex-col w-40 gap-6 p-4 m-4">
+        <h1 className="text-lg text-red-700">Counter</h1>
+
+        <h1 className="p-4 rounded-lg bg-slate-500">Count: {count}</h1>
+        <button className="btn" onClick={increment}>
+          +Increment
+        </button>
+        <button className="btn" onClick={decrement}>
+          -Decrement
+        </button>
+        <button className="btn" onClick={reset}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+}
