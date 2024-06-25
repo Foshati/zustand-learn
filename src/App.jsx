808ccf0b4d26d96fcfb29fa2f15c244f4b93bd1a
Foshati <amirrezafoshati@gmail.com>
2024-06-25 23:47:21 +0330

    create counter with zustand

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import useCounterStore from "./store/useCounterStore";
+
 function App() {
+  const { count, increment, decrement, reset } = useCounterStore();
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello fa!</h1>
-      <button className="btn">Button</button>
+      <div className="flex flex-col w-3/12 gap-6 p-4 m-4">
+        <h1 className="p-4 rounded-lg bg-slate-500">Count: {count}</h1>
+        <button className="btn" onClick={increment}>
+          +Increment
+        </button>
+        <button className="btn" onClick={decrement}>
+          -Decrement
+        </button>
+        <button className="btn" onClick={reset}>
+          Reset
+        </button>
+      </div>
     </>
   );
 }
