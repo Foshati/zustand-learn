662f316da0ef90b10113709d110daa6f28e843f2
Foshati <amirrezafoshati@gmail.com>
2024-06-25 23:22:18 +0330

    start project vite and react and tailwindCss and daisy ui and zustand

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello fa!</h1>
+      <button className="btn">Button</button>
+    </>
+  );
+}
+
+export default App;
