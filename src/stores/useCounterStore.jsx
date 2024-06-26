dc4379025e4b30100a0e83cbf3d0adc1afbf2019
Foshati <amirrezafoshati@gmail.com>
2024-06-26 14:21:03 +0330

    save to localStorage for useCounterStore

--- src/stores/useCounterStore.jsx
+++ src/stores/useCounterStore.jsx
@@ -1,8 +1,17 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useCounterStore = create((set) => ({
-  count: 0,
-  increment: () => set((state) => ({ count: state.count + 1 })),
-  decrement: () => set((state) => ({ count: state.count - 1 })),
-  reset: () => set({ count: 0 }),
-}));
+export const useCounterStore = create(
+  persist(
+    (set) => ({
+      count: 0,
+      increment: () => set((state) => ({ count: state.count + 1 })),
+      decrement: () => set((state) => ({ count: state.count - 1 })),
+      reset: () => set({ count: 0 }),
+    }),
+    {
+      name: "count-storage",
+      getStorage: () => localStorage,
+    }
+  )
+);
